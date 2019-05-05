b30aaeac1fff4e2e813242820636a37f90b08fb2
Gabriel Rodrigues <gabrielws31@gmail.com>
2019-05-05 17:43:24 -0300

    Views de Clientes quase prontos

--- skynet/urls.py
+++ skynet/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from . import views
 from clientes import urls as clients_urls
@@ -26,6 +26,6 @@ urlpatterns = [
     path('clients/', include(clients_urls)),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
