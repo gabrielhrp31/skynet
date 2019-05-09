a63486a4a8fd6126e2c78a9df0ea8fb0b8842dbb
Gabriel Rodrigues <gabrielws31@gmail.com>
2019-05-09 19:33:45 -0300

    Dashboard de clientes e parte de clientes concluida

--- skynet/views.py
+++ skynet/views.py
@@ -15,15 +15,22 @@ def index(request):
     # Calcula a porcentagem de clientes cadastrados
     all_recents=Client.objects.filter(created_at__lte=datetime.now(tz=timezone.utc)-timedelta(30))
     all_recents = all_recents.count()
-    all_recents=((all-all_recents)/all)*100
+    if all_recents>0:
+        all_recents=((all-all_recents)/all)*100
 
     # Pega a quantidade de clientes ativos e calcula a porcentagem em relação ao total
     list_actives = Client.objects.filter(status=True)
     list_actives = list_actives.count()
-    actives = {'amount': list_actives, 'percentual': ((list_actives/all)*100)}
+    if list_actives>0:
+        actives = {'amount': list_actives, 'percentual': ((list_actives/all)*100)}
+    else:
+        actives = {'amount': 0, 'percentual': 0}
 
     # Pega a quantidade de clientes inativos e calcula a porcentagem em relação ao total
     list_inactives = Client.objects.filter(status=False)
     list_inactives = list_inactives.count()
-    inactives = {'amount': list_inactives, 'percentual': ((list_inactives/all)*100)}
+    if list_inactives>0:
+        inactives = {'amount': list_inactives, 'percentual': ((list_inactives/all)*100)}
+    else:
+        inactives = {'amount': 0, 'percentual': 0}
     return render(request, 'dashboard/index.html', {'all': all, 'all_recents': all_recents, 'actives': actives, 'inactives': inactives})
